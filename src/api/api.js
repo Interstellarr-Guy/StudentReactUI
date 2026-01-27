04feaf9a0cca944841cf4852378953dd9d96e89e
Deva kumar <rajmechn@gmail.com>
2026-01-27 14:18:16 +0530

    axios manual changed to auto

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 //  Interceptor
-api.interceptors.request.use(config => {
+axios.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = "Bearer " + token;
