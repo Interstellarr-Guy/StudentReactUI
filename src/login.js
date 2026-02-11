01ad71c626640c2ea029f0aac75cb952cccf4774
Interstellar Guy <interstellarguy29@gmail.com>
2026-02-11 14:36:33 +0530

    Update login API endpoint URL

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 export async function login(username, password) {
-  const response = await fetch("https://stuapi-production.up.railway.app/auth/login", {
+  const response = await fetch("https://stuapi-s7jc.onrender.com/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
